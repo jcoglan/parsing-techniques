0c7ad9995296193e45e15acff5a974d66db066bd
James Coglan <jcoglan@gmail.com>
2017-07-12 18:49:55 +0100

    Implement most of the building blocks for conversion to CNF: removing epsilon and unit rules.

--- lib/grammar.rb
+++ lib/grammar.rb
@@ -158,6 +158,88 @@ Grammar = Struct.new(:rules) do
     reachable_rules = rules.find_all { |rule| reachable_terms.member?(rule.lhs) }
     Grammar.new(reachable_rules)
   end
+
+  def to_cnf
+    split_choices.remove_epsilon_rules.remove_unit_rules.clean
+  end
+
+  def remove_epsilon_rules
+    primes    = {}
+    new_rules = rules
+
+    Algo.closure(find_empties(rules)) do |known, new|
+      new_rules = new.inject(new_rules) do |rules, symbol|
+        primed = NonTerminal.new(symbol.name + "'")
+        primes[symbol] = primed
+
+        rules.flat_map do |rule|
+          split_rule_on_empty(rule, symbol, primed)
+        end
+      end
+      find_empties(new_rules)
+    end
+
+    primed_rules = primes.flat_map do |from, to|
+      rules = new_rules.find_all { |rule| rule.lhs == from and rule.rhs != Empty }
+      rules.map { |rule| Rule.new(to, rule.rhs) }
+    end
+
+    new_rules = Set.new(new_rules + primed_rules).entries
+
+    unknown_keys = primes.values - primed_rules.map(&:lhs)
+    new_rules.delete_if { |rule| unknown_keys.include? rule.rhs }
+
+    Grammar.new(new_rules)
+  end
+
+  def remove_unit_rules
+    new_rules = rules
+
+    Algo.closure(find_units(rules)) do |known, new|
+      new_rules = new_rules.flat_map do |rule|
+        next [rule] if Sequence === rule.rhs or Terminal === rule.rhs
+        next [] if rule.lhs == rule.rhs
+
+        new_rules.find_all { |r| r.lhs == rule.rhs }.map do |right|
+          Rule.new(rule.lhs, right.rhs)
+        end
+      end
+      find_units(new_rules)
+    end
+
+    Grammar.new(new_rules)
+  end
+
+private
+
+  def find_units(rules)
+    rules.reject { |rule| Sequence === rule.rhs }
+  end
+
+  def find_empties(rules)
+    rules.find_all { |rule| rule.rhs == Empty }.map(&:lhs)
+  end
+
+  def split_rule_on_empty(rule, symbol, primed)
+    return [rule] unless rule.rhs.entries.include?(symbol)
+
+    if rule.rhs == symbol
+      return [primed, Empty].map { |rhs| Rule.new(rule.lhs, rhs) }
+    end
+
+    [].tap do |rules|
+      primed_items = rule.rhs.items.map { |t| t == symbol ? primed : t }
+      rules << Rule.new(rule.lhs, Sequence.new(primed_items))
+
+      indexes = primed_items.each_index.find_all { |i| primed_items[i] == primed }
+
+      indexes.each do |index|
+        reduced = primed_items.take(index) + primed_items.drop(index + 1)
+        reduced = (reduced.size == 1) ? reduced.first : Sequence.new(reduced)
+        rules << Rule.new(rule.lhs, reduced)
+      end
+    end
+  end
 end
 
 require_relative './grammar/builder'
