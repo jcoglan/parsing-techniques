405c6cd6873d680c1d38b189286d7722c15f5782
James Coglan <jcoglan@gmail.com>
2017-07-12 18:50:09 +0100

    Some tests for removing epsilon rules.

--- spec/grammar_spec.rb
+++ spec/grammar_spec.rb
@@ -24,4 +24,45 @@ describe Grammar do
       GMR
     end
   end
+
+  describe :remove_epsilon_rules do
+    it 'removes epsilon rules from a simple grammar' do
+      grammar = Grammar.parse <<-GMR
+        S -> L a M
+        L -> L M | ε
+        M -> M M | ε
+      GMR
+      grammar.split_choices.remove_epsilon_rules.join_choices.must_equal Grammar.parse(<<-GMR)
+        S  -> L' a M' | L' a | a M' | a
+        L  -> L' M' | L' | M' | ε
+        M  -> M' M' | M' | ε
+        L' -> L' M' | L' | M'
+        M' -> M' M' | M'
+      GMR
+    end
+
+    it 'removes epsilon rules from a more complex grammar' do
+      grammar = Grammar.parse <<-GMR
+        Number   -> Integer | Real
+        Integer  -> Digit | Integer Digit
+        Real     -> Integer Fraction Scale
+        Fraction -> . Integer
+        Scale    -> e Sign Integer | Empty
+        Digit    -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+        Sign     -> + | -
+        Empty    -> ε
+      GMR
+      grammar.split_choices.remove_epsilon_rules.join_choices.must_equal Grammar.parse(<<-GMR)
+        Number   -> Integer | Real
+        Integer  -> Digit | Integer Digit
+        Real     -> Integer Fraction Scale' | Integer Fraction
+        Fraction -> . Integer
+        Scale    -> e Sign Integer | ε
+        Digit    -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+        Sign     -> + | -
+        Empty    -> ε
+        Scale'   -> e Sign Integer
+      GMR
+    end
+  end
 end
